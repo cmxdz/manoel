16eaf43718d4c3cc9f09310903477c922be89801
cmxdz <125364576+cmxdz@users.noreply.github.com>
2023-04-05 09:19:57 -0300

    Add files via upload
    aula13 que deu erro

---
+++ aula12/listagem.php
@@ -0,0 +1,31 @@
+<?php
+    include_once "dados.php";
+    include_once "header.php";
+    include_once "menu.php";
+?>
+
+
+
+    
+        <!-- Noticias -->
+<div class="container">
+  <div class="row row-cols-1 row-cols-md-3 g-3">
+
+        <?php foreach($noticias as $noticia): ?>
+          <div class="card">
+              <img src="<?php echo $noticia['foto']; ?> "class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title"> <?php echo $noticia['titulo']; ?></h5>
+                  <p class="card-text"> <?php echo $noticia['chamada']; ?></p>
+                  <a href="#" class="btn btn-primary">Ler mais</a>
+                </div>
+          </div>
+        <?php endforeach; ?>
+  </div>
+</div>
+        
+        <!-- Fim Noticias -->
+
+        <?php
+                include_once "rodape.php";
+                ?>
