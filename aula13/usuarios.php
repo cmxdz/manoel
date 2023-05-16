c8e598c29a2fbd39f55f45981051c9bb4c001886
cmxdz <ariel.floriano@alunos.ifsuldeminas.edu.br>
2023-05-16 08:41:33 -0300

    aula16

--- aula13/usuarios.php
+++ aula13/usuarios.php
@@ -5,8 +5,9 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Usuários</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   </head>
   <body>
 
@@ -29,7 +30,11 @@
             <tr>
                 <td> <?php echo $usuario-> nome ?>  </td>
                 <td> <?php echo $usuario-> login ?> </td>
-                <td>@mdo</td>
+                <td>
+                  <a href="usuario/excluir.php?id=<?= $usuario->idusuario ?>" class="btn btn-danger"> <i class="fa-sharp fa-solid fa-trash"></i> Excluir </a>
+
+                  <a href="form_usuario.php?id=<?= $usuario->idusuario ?>" class="btn btn-primary"> <i class="fa-sharp fa-solid fa-pen-to-square"></i> Atualizar </a>
+                </td>
             </tr>
             <?php endforeach; ?>
   </tbody>
